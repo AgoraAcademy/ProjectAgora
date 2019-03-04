94ef0786a27b8f844ba0db828673310d6c1fcbf3
xct24 <xct24@live.com>
2019-03-04 08:42:25 +0800

    修复bug：更新本地更新token的流程

--- src/util.tsx
+++ src/util.tsx
@@ -17,48 +17,50 @@ export function fetchRequest(url, method, params: any = '') {
         "openid": window.localStorage.getItem("openid"), //用户登陆后返回的token，某些涉及用户数据的接口需要在header中加上token
     };
     console.log('request url:', url, params); //打印请求参数
-    if (params == '') { //如果网络请求中带有参数
+    if (params == '') { //如果没有body
         return new Promise(function (resolve, reject) {
             fetch(SERVERURL + url, {
                 method: method,
                 headers: header
-            }).then((response) => response.json())
-                .then((responseData) => {
-                    console.log('res:', url, responseData); //网络请求成功返回的数据
-                    try{
-                        window.localStorage.setItem("access_token", responseData.headers.get("Authorization"))
-                        window.localStorage.setItem("refresh_token", responseData.headers.get("refresh_token"))}
-                    catch(err){
-                        console.log(err)
-                    }
-                    resolve(responseData);
-                })
-                .catch((err) => {
-                    console.log('err:', url, err); //网络请求失败返回的数据
-                    reject(err);
-                });
+            })
+            .then((response) => {
+                try{
+                    window.localStorage.setItem("access_token", response.headers.get("Authorization"))
+                    window.localStorage.setItem("refresh_token", response.headers.get("refresh_token"))}
+                catch(err){
+                    console.log(err)
+                }
+                return response.json()
+            })
+            .then((responseData) => {
+                resolve(responseData);
+            })
+            .catch((err) => {
+                reject(err);
+            });
         });
-    } else { //如果网络请求中没有参数
+    } else { //如果有body
         return new Promise(function (resolve, reject) {
             fetch(SERVERURL + url, {
                 method: method,
                 headers: header,
                 body: JSON.stringify(params) //body参数，通常需要转换成字符串后服务器才能解析
-            }).then((response) => response.json())
-                .then((responseData) => {
-                    console.log('res:', url, responseData); //网络请求成功返回的数据
-                    try{
-                        window.localStorage.setItem("access_token", responseData.headers.get("Authorization"))
-                        window.localStorage.setItem("refresh_token", responseData.headers.get("refresh_token"))}
-                    catch(err){
-                        console.log(err)
-                    }
-                    resolve(responseData);
-                })
-                .catch((err) => {
-                    console.log('err:', url, err); //网络请求失败返回的数据
-                    reject(err);
-                });
+            })
+            .then((response) => {
+                try{
+                    window.localStorage.setItem("access_token", response.headers.get("Authorization"))
+                    window.localStorage.setItem("refresh_token", response.headers.get("refresh_token"))}
+                catch(err){
+                    console.log(err)
+                }
+                return response.json()
+            })
+            .then((responseData) => {
+                resolve(responseData);
+            })
+            .catch((err) => {
+                reject(err);
+            });
         });
     }
 }
