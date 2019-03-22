c6f30509f6ab64ec3cecf2391cbd72f0ed12e3a3
xct24 <xct24@live.com>
2019-03-22 22:25:50 +0800

    缺省值

--- src/components/Content/ProjectList.tsx
+++ src/components/Content/ProjectList.tsx
@@ -49,7 +49,11 @@ class ProjectList extends React.Component<IProjectListProps> {
             projectList = projectList.filter(project => project.status === "未提交")
         }
         let list = projectList.map((item, index) => {
-            const coverImageData = fetchRequest(`/v1/utilities/project_cover?learnerId=${window.localStorage.getItem("learnerId")}&uid=${item.CoverImageURL}`, "GET").projectCover
+            if (item.CoverImageURL) {
+                const coverImageData = fetchRequest(`/v1/utilities/project_cover?learnerId=${window.localStorage.getItem("learnerId")}&uid=${item.CoverImageURL}`, "GET").then((response: any) => response.projectCover)
+            } else {
+                const coverImageData = ""
+            }
             return (
                 <ProjectCard
                     key={`ProjectCard_${index}`}
