4ca2a01ab21b6577a1b7958c06d725ce75cb6614
xct24 <xct24@live.com>
2019-05-14 19:10:40 +0800

    处理#20： 为MDEditor顶上添加一个divider显示字段类型，并且添加了一些简短的介绍。

--- src/components/Widget/MDEditor.tsx
+++ src/components/Widget/MDEditor.tsx
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import SimpleMDEReact from "react-simplemde-editor";
+import * as PropTypes from "prop-types";
 import './MDEditor.less'
 import { connect } from "dva";
 import { toggleSideBySide, togglePreview } from "simplemde";
+import { Divider, Tooltip } from "antd";
+import { Icon } from "react-uwp";
 
 interface IMDEditorProps {
     itemType: string,
@@ -20,6 +23,8 @@ interface IMDEditorProps {
  * @extends {Component<IMDEditor>}
  */
 class MDEditor extends Component<IMDEditorProps> {
+    public static contextTypes = { theme: PropTypes.object };
+    public context: { theme: ReactUWP.ThemeType };
     public getMdeInstance = (instance) =>{
         const { pushToInstanceList } = this.props
         instance.togglePreview()
@@ -38,13 +43,65 @@ class MDEditor extends Component<IMDEditorProps> {
         "preview", "guide"
     ]
 
+    public getDivider = (itemType: string) => {
+        const { theme } = this.context;
+        if (itemType === "itemContent") {
+            return (
+                <Divider
+                    style={{ color: 'white', ...theme.typographyStyles.title }}
+                >
+                    内容
+                    <Tooltip
+                        placement="bottom"
+                        title="此处填写项目计划要进行的活动内容"
+                    >
+                        <Icon style={{marginLeft: "10px"}}>Info</Icon>
+                    </Tooltip>
+                </Divider>
+            )
+        }
+        if (itemType === "itemRecord") {
+            return (
+                <Divider
+                    style={{ color: 'white', ...theme.typographyStyles.title }}
+                >
+                    记录
+                    <Tooltip
+                        placement="bottom"
+                        title="此处填写项目进行的实际情况记录，包括进展、感想、学时记录等"
+                    >
+                        <Icon style={{marginLeft: "10px"}}>Info</Icon>
+                    </Tooltip>
+                </Divider>
+            )
+        }
+        if (itemType === "itemComment") {
+            return (
+                <Divider
+                    style={{ color: 'white', ...theme.typographyStyles.title }}
+                >
+                    反馈
+                    <Tooltip
+                        placement="bottom"
+                        title="此处由项目导师填写，包括对项目进展提出反馈意见和认证学时等"
+                    >
+                        <Icon style={{marginLeft: "10px"}}>Info</Icon>
+                    </Tooltip>
+                </Divider>
+            )
+        }
+    }
+
     public render() {
         const { value } = this.props
+        const { theme } = this.context;
         if (!this.props.pushToInstanceList) {
             this.getMdeInstance = () => {return}
         }
         return (
-            <SimpleMDEReact
+            <div>
+                {this.getDivider(this.props.itemType)}
+                <SimpleMDEReact
                 getMdeInstance={this.getMdeInstance}
                 className={'CodeMirror'}
                 onChange={this.handleChange}
@@ -61,6 +118,8 @@ class MDEditor extends Component<IMDEditorProps> {
                     toolbar: this.toolbarIcons
                 }}
             />
+            </div>
+            
         );
     }
 }
